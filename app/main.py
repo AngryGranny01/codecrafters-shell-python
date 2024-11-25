13b4ddeda91937fe0e343324469f39cb5dc59d20
rodie01 <rojo.dienst@gmail.com>
2024-11-25 15:06:05 +0100

    type PATH

--- app/main.py
+++ app/main.py
@@ -50,7 +50,6 @@ def handle_type(args):
 
 def handle_directory_search(cmd):
     directories = path_env.split(":")
-    print(directories)
     for directory in directories:
         execFile = directory.split("/")
         if(execFile == cmd):
